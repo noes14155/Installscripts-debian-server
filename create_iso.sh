831463da666d90717f185406741cf9a324b0163b
Noel Samuel <noes14155@gmail.com>
2023-10-09 06:21:37 +0400

    Update create_iso.sh

--- create_iso.sh
+++ create_iso.sh
@@ -6,4 +6,5 @@ gunzip isofiles/install.amd/initrd.gz
 echo preseed.cfg | cpio -H newc -o -A -F isofiles/install.amd/initrd
 gzip isofiles/install.amd/initrd
 chmod -w -R isofiles/install.amd/
+cp setup.sh isofiles/
 genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o debian-12-unattended.iso isofiles
